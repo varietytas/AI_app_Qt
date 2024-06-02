ecd77a42fed327358744028b4a700d507a1fc5df
sergeybudygin <svbudygin@yandex.ru>
2024-06-02 12:28:08 +0300

    fix bug port

--- requestsToBackend.h
+++ requestsToBackend.h
@@ -29,7 +29,7 @@ public:
     QJsonObject get_token();
   
 private:
-    QString apiUrl= "http://127.0.0.1:8000";
+    QString apiUrl= "http://62.113.113.54:8000";
     QJsonObject send_request(const QJsonObject& json_obj, const QString& url, const QString& method);
 
     
